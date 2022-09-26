d8cb1ab0ab089dca4223014224d8103fb454a0ee
michelemiko1 <61735529+michelemiko1@users.noreply.github.com>
2022-09-26 12:45:57 +0200

    grass added

--- poke_main.py
+++ poke_main.py
@@ -1,7 +1,32 @@
 import pygame
 
 
+WIDTH, HEIGHT = 1280, 720
+FPS = 60
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+GRASS = pygame.image.load('images/grass.jpg')
+GRASS = pygame.transform.scale(GRASS, (WIDTH, HEIGHT))
+
+def draw_window():
+    WIN.blit(GRASS, (0, 0))
+    pygame.display.update()
+
+
+
 if __name__ == '__main__':
 
-    print('Paolo')
-    print('Lorenzo')
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+        clock.tick(FPS)
+        draw_window()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+
+
